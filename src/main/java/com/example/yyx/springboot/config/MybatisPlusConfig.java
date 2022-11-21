f1ff6e1f411b92a023cf3807ede8c061c1a1874d
slamd14 <446973181@qq.com>
2022-11-21 21:59:13 +0800

    Update MybatisPlusConfig.java

--- src/main/java/com/example/yyx/springboot/config/MybatisPlusConfig.java
+++ src/main/java/com/example/yyx/springboot/config/MybatisPlusConfig.java
@@ -2,7 +2,6 @@ package com.example.yyx.springboot.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
